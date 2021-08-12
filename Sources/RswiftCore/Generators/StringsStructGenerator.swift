b260e97a89a366b750738fd1a53723c4dc5a22f4
Michael Rublev <michrublev@gmail.com>
2021-08-13 04:13:26 +0600

    Add ability to pass preferred languages via UserDefauls with custom key "rswift.lang".

--- Sources/RswiftCore/Generators/StringsStructGenerator.swift
+++ Sources/RswiftCore/Generators/StringsStructGenerator.swift
@@ -247,7 +247,7 @@ struct StringsStructGenerator: ExternalOnlyStructGenerator {
         Function.Parameter(
           name: "preferredLanguages",
           type: Type._Array.withGenericArgs([Type._String]).asOptional(),
-          defaultValue: "nil"
+          defaultValue: "UserDefaults.standard.array(forKey: \"rswift.lang\") as? [String]"
         )
       ],
       doesThrow: false,
@@ -282,7 +282,7 @@ struct StringsStructGenerator: ExternalOnlyStructGenerator {
     let prefereredLanguages = Function.Parameter(
       name: "preferredLanguages",
       type: Type._Array.withGenericArgs([Type._String]).asOptional(),
-      defaultValue: "nil"
+      defaultValue: "UserDefaults.standard.array(forKey: \"rswift.lang\") as? [String]"
     )
     params.append(prefereredLanguages)
 
